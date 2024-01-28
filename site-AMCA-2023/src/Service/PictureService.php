afe1c61c378cc1b3e82618bab0eb44df96f73b02
Julia-Legrand <julia-legrand@orange.fr>
2024-01-28 19:38:56 +0100

    Traduction des commentaires en anglais

--- site-AMCA-2023/src/Service/PictureService.php
+++ site-AMCA-2023/src/Service/PictureService.php
@@ -16,17 +16,17 @@ class PictureService
 
     public function add(UploadedFile $tripPicture, ?string $folder = '', ?int $width = 200, ?int $height = 200)
     {
-        // Nouveau nom pour les images
+        // Rename new pictures
         $fichier = md5(uniqid(rand(), true)) . '.png';
 
-        // Obtention des informations sur l'image
+        // Get informations about picture
         $tripPictureInfos = getimagesize($tripPicture->getPathname());
 
         if ($tripPictureInfos === false) {
             throw new \Exception('Format d\'image incorrect');
         }
 
-        // Vérification du format de l'image
+        // Check format
         switch ($tripPictureInfos['mime']) {
             case 'image/png':
                 $tripPictureSource = imagecreatefrompng($tripPicture->getPathname());
@@ -39,21 +39,21 @@ class PictureService
                 throw new \Exception('Format d\'image incorrect');
         }
 
-        // Redimensionnement de l'image
+        // Resize picture
         $imageWidth = $tripPictureInfos[0];
         $imageHeight = $tripPictureInfos[1];
 
-        // Création de la nouvelle image
+        // Create new resized picture
         $resizedPicture = imagecreatetruecolor($width, $height);
         imagecopyresampled($resizedPicture, $tripPictureSource, 0, 0, 0, 0, $width, $height, $imageWidth, $imageHeight);
 
-        // Chemin pour stocker l'image redimensionnée
+        // Path to store resized picture
         $path = $this->params->get('images_directory') . $folder;
 
-        // Stockage de l'image redimensionnée au format PNG
+        // Store resized picture in png format
         imagepng($resizedPicture, $path . $width . 'x' . $height . '-' . $fichier);
 
-        // Déplacement de l'image originale vers le répertoire de destination
+        // Move original picture to destination folder
         $tripPicture->move($path . '/', $fichier);
 
         return $fichier;
