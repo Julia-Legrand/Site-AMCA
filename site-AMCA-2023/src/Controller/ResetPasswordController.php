c8b0d37c427d30eeb3bcc02c9864a3addd1f7f79
Julia-Legrand <julia-legrand@orange.fr>
2024-02-01 16:55:12 +0100

    Configuration controller resetPassword

--- site-AMCA-2023/src/Controller/ResetPasswordController.php
+++ site-AMCA-2023/src/Controller/ResetPasswordController.php
@@ -170,7 +170,7 @@ class ResetPasswordController extends AbstractController
         }
 
         $email = (new TemplatedEmail())
-            ->from(new Address('Email à configurer', 'Administrateur du site'))
+            ->from(new Address('amca44@julia-legrand.fr', 'Administrateur du site'))
             ->to($user->getEmail())
             ->subject('Votre demande de réinitialisation de mot de passe')
             ->htmlTemplate('reset_password/email.html.twig')
