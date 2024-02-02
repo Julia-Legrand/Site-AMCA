6ff04fcd2e1b0fc3f7935c642bc687ee29c4bbb9
Julia-Legrand <julia-legrand@orange.fr>
2024-02-02 16:44:03 +0100

    Début des tests unitaires

---
+++ site-AMCA-2023/tests/Controller/MainControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class MainControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertResponseStatusCodeSame(200);
+    }
+}
