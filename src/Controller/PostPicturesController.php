d49aa0535473dfc27f7c9383f7f5ff9949677bb2
Julia-Legrand <julia-legrand@orange.fr>
2024-05-06 18:19:50 +0200

    Correction d'un controller

--- src/Controller/PostPicturesController.php
+++ src/Controller/PostPicturesController.php
@@ -12,7 +12,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\String\Slugger\SluggerInterface;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/photo-post')]
